448e1044b334d68e2a945c446b2f75e7a1cbe45c
dav <contact@daviani.dev>
2023-08-03 14:50:45 +0200

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,53 +17,26 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          50: "#eff6ff",
-          100: "#dbeafe",
-          200: "#bfdbfe",
-          300: "#93c5fd",
-          400: "#60a5fa",
-          500: "#3b82f6",
-          600: "#2563eb",
-          700: "#1d4ed8",
-          800: "#1e40af",
-          900: "#1e3a8a",
+        main: {
+          800: '#2E3440',
+          700: '#4C566A',
+          200: '#D8DEE9',
+          100: '#FCFDFF',
+          50 : '#E5E9F0'
         },
+        sun : {
+          800: '#B48EAD',
+          500: '#BF616A',
+          300: '#EBCB8B'
+        },
+        blue: {
+          600: '#5E81AC',
+          300: '#8FBCBB'
+        }
       },
       fontFamily: {
         body: [
-          "Inter",
-          "ui-sans-serif",
-          "system-ui",
-          "-apple-system",
-          "system-ui",
-          "Segoe UI",
           "Roboto",
-          "Helvetica Neue",
-          "Arial",
-          "Noto Sans",
-          "sans-serif",
-          "Apple Color Emoji",
-          "Segoe UI Emoji",
-          "Segoe UI Symbol",
-          "Noto Color Emoji",
-        ],
-        sans: [
-          "Inter",
-          "ui-sans-serif",
-          "system-ui",
-          "-apple-system",
-          "system-ui",
-          "Segoe UI",
-          "Roboto",
-          "Helvetica Neue",
-          "Arial",
-          "Noto Sans",
-          "sans-serif",
-          "Apple Color Emoji",
-          "Segoe UI Emoji",
-          "Segoe UI Symbol",
-          "Noto Color Emoji",
         ],
       },
       borderWidth: {
